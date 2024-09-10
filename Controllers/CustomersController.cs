90d8556796a2e607eeee6f990963fae4ab6d049f
Toni Pekkala <toni.pekkala@hotmail.com>
2024-09-10 13:33:27 +0300

    eka commit

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -15,9 +15,37 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers() 
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " +e.InnerException);
+            }
+
         }
+
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);                
+                return Ok(asiakas);
+            }
+
+            catch (Exception e)
+
+            {
+              //return BadRequest("Asiakasta id:llä" + id "ei löydy");
+              return BadRequest($"Asiakasta id:llä {id} ei löydy. Lue lisää: "+e.InnerException);//string interpolation
+            }
+
+        }
+
+     
 
     }
 }
