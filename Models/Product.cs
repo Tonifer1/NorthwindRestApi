7188717be0b01976f1e834ed1379ee6e7bdad60a
Toni Pekkala <toni.pekkala@hotmail.com>
2024-10-28 00:34:09 +0200

    Lisätty UserController ja päivitetty models kansio.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace NorthwindRestApi.Models
 {
@@ -13,13 +12,8 @@ namespace NorthwindRestApi.Models
 
         public int ProductId { get; set; }
         public string ProductName { get; set; } = null!;
-
-        [JsonIgnore]
         public int? SupplierId { get; set; }
-
-        [JsonIgnore]
         public int? CategoryId { get; set; }
-
         public string? QuantityPerUnit { get; set; }
         public decimal? UnitPrice { get; set; }
         public short? UnitsInStock { get; set; }
@@ -28,13 +22,8 @@ namespace NorthwindRestApi.Models
         public bool Discontinued { get; set; }
         public string? Imagelink { get; set; }
 
-        [JsonIgnore]
         public virtual Category? Category { get; set; }
-
-        [JsonIgnore]
         public virtual Supplier? Supplier { get; set; }
-
-        [JsonIgnore]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindRestApi.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int AcceslevelId { get; set; }
+    }
+}
